cd14e1d5a463a806ec9e85a42374614fd94d7237
Ian Anderson <ianderson@academicsoftwareplus.com>
2011-12-07 14:57:24 -0500

    accept parameters

--- lib/madweblibs.rb
+++ lib/madweblibs.rb
@@ -6,10 +6,10 @@ module Madweblibs
     DEFAULT_ADJECTIVES_COUNT = 3
     DEFAULT_ADVERB_PROBABILITY = 0.20
 
-    def generate
-      generator = Generator.new(adjective_count = DEFAULT_ADJECTIVES_COUNT,
-                                adverb_probability = DEFAULT_ADVERB_PROBABILITY)
-      puts generator.generate
+    def generate(adjective_count = DEFAULT_ADJECTIVES_COUNT,
+                 adverb_probability = DEFAULT_ADVERB_PROBABILITY)
+      generator = Generator.new(adjective_count, adverb_probability)
+      generator.generate
     end
   end
 end
